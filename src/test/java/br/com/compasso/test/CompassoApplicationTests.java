f2faefe6068c4e98ac1983c84281ae7e5518ef6b
Fernando Melo <fernando.o.melo@gmail.com>
2020-08-22 11:16:27 -0300

    Add basic project structure

---
+++ src/test/java/br/com/compasso/test/CompassoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
